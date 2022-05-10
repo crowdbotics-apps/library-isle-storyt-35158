0e8a7c445a154342efd93c152d1f1348e892ae69
Crowdbotics <team@crowdbotics.com>
2022-05-10 12:44:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/library_isle_storyt_35158/MainActivity.java
@@ -0,0 +1,15 @@
+package com.library_isle_storyt_35158;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "library_isle_storyt_35158";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "library_isle_storyt_35158",
+  url: "https://library_isle_storyt_35158.botics.co",
+  api: "https://library_isle_storyt_35158.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
